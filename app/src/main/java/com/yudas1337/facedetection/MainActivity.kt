f0e985b79638940e9a748667df15e767a74655e2
Yudas <yudasmalabi@gmail.com>
2024-01-15 23:10:39 +0700

    Basic Face Detection Google Ml Kit Using Landscape Orientation

---
+++ app/src/main/java/com/yudas1337/facedetection/MainActivity.kt
@@ -0,0 +1,120 @@
+package com.yudas1337.facedetection
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.util.Size
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.ImageProxy
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.face.FaceDetection
+import com.google.mlkit.vision.face.FaceDetector
+import com.google.mlkit.vision.face.FaceDetectorOptions
+import com.yudas1337.facedetection.databinding.ActivityMainBinding
+import java.util.concurrent.Executors
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var cameraSelector: CameraSelector
+    private lateinit var processCameraProvider: ProcessCameraProvider
+    private lateinit var cameraPreview: Preview
+    private lateinit var imageAnalysis: ImageAnalysis
+
+    private val previewWidth: Int = 1920
+    private val previewHeight: Int = 1440
+
+    private val cameraXViewModel = viewModels<CameraModel>()
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        cameraSelector =
+            CameraSelector.Builder().requireLensFacing(CameraSelector.LENS_FACING_FRONT).build()
+        cameraXViewModel.value.processCameraProvider.observe(this) { provider ->
+            processCameraProvider = provider
+            bindCameraPreview()
+            bindInputAnalyser()
+        }
+    }
+
+    private fun bindCameraPreview() {
+
+        cameraPreview = Preview.Builder()
+            .setTargetResolution(Size(previewWidth, previewHeight))
+            .setTargetRotation(binding.previewView.display.rotation)
+            .build()
+        cameraPreview.setSurfaceProvider(binding.previewView.surfaceProvider)
+        try {
+            processCameraProvider.bindToLifecycle(this, cameraSelector, cameraPreview)
+        } catch (illegalStateException: IllegalStateException) {
+            Log.e(TAG, illegalStateException.message ?: "IllegalStateException")
+        } catch (illegalArgumentException: IllegalArgumentException) {
+            Log.e(TAG, illegalArgumentException.message ?: "IllegalArgumentException")
+        }
+    }
+
+    private fun bindInputAnalyser() {
+        val detector = FaceDetection.getClient(
+            FaceDetectorOptions.Builder()
+                .setPerformanceMode(FaceDetectorOptions.PERFORMANCE_MODE_FAST)
+                .setContourMode(FaceDetectorOptions.CONTOUR_MODE_NONE)
+                .build()
+        )
+        imageAnalysis = ImageAnalysis.Builder()
+            .setTargetRotation(binding.previewView.display.rotation)
+            .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+            .build()
+
+        val cameraExecutor = Executors.newSingleThreadExecutor()
+
+        imageAnalysis.setAnalyzer(cameraExecutor) { imageProxy ->
+            processImageProxy(detector, imageProxy)
+        }
+
+        try {
+            processCameraProvider.bindToLifecycle(this, cameraSelector, imageAnalysis)
+        } catch (illegalStateException: IllegalStateException) {
+            Log.e(TAG, illegalStateException.message ?: "IllegalStateException")
+        } catch (illegalArgumentException: IllegalArgumentException) {
+            Log.e(TAG, illegalArgumentException.message ?: "IllegalArgumentException")
+        }
+    }
+
+    @SuppressLint("UnsafeOptInUsageError")
+    private fun processImageProxy(detector: FaceDetector, imageProxy: ImageProxy) {
+        val inputImage =
+            InputImage.fromMediaImage(imageProxy.image!!, imageProxy.imageInfo.rotationDegrees)
+        detector.process(inputImage).addOnSuccessListener { faces ->
+            binding.graphicOverlay.clear()
+            if(faces.isNotEmpty()){
+                val face = faces[0]
+                val faceBox = FaceBox(binding.graphicOverlay, face, imageProxy.image!!.cropRect)
+                binding.graphicOverlay.add(faceBox)
+            }
+        }.addOnFailureListener {
+            it.printStackTrace()
+        }.addOnCompleteListener {
+            imageProxy.close()
+        }
+    }
+
+    companion object {
+        private val TAG = MainActivity::class.simpleName
+        fun startActivity(context: Context) {
+            Intent(context, MainActivity::class.java).also {
+                context.startActivity(it)
+            }
+        }
+    }
+}
